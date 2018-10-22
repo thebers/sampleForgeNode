5793579b895228e6c9ac15f52e9e0b5473275b0b
Ben Bringardner <ben.bringardner@gmail.com>
2018-10-22 15:53:17 -0500

    Toolbar Extension Working Now

---
+++ public/js/bensawesomeextension.js
@@ -0,0 +1,66 @@
+// *******************************************
+// My Awesome Extension
+// *******************************************
+function bensAwesomeExtension(viewer, options) {
+  Autodesk.Viewing.Extension.call(this, viewer, options);
+}
+
+bensAwesomeExtension.prototype = Object.create(Autodesk.Viewing.Extension.prototype);
+bensAwesomeExtension.prototype.constructor = bensAwesomeExtension;
+
+bensAwesomeExtension.prototype.load = function () {
+  if (this.viewer.toolbar) {
+    // Toolbar is already available, create the UI
+    this.createUI();
+  } else {
+    // Toolbar hasn't been created yet, wait until we get notification of its creation
+    this.onToolbarCreatedBinded = this.onToolbarCreated.bind(this);
+    this.viewer.addEventListener(Autodesk.Viewing.TOOLBAR_CREATED_EVENT, this.onToolbarCreatedBinded);
+  }
+  return true;
+};
+
+bensAwesomeExtension.prototype.onToolbarCreated = function () {
+  this.viewer.removeEventListener(Autodesk.Viewing.TOOLBAR_CREATED_EVENT, this.onToolbarCreatedBinded);
+  this.onToolbarCreatedBinded = null;
+  this.createUI();
+};
+
+bensAwesomeExtension.prototype.createUI = function () {
+  var _this = this;
+
+  // prepare to execute the button action
+  var myAwesomeToolbarButton = new Autodesk.Viewing.UI.Button('runMyAwesomeCode');
+  myAwesomeToolbarButton.onClick = function (e) {
+
+    // **********************
+    //
+    //
+    // Execute an action here
+    //
+    //
+    // **********************
+
+    alert('I am an extension');
+
+  };
+  // myAwesomeToolbarButton CSS class should be defined on your .css file
+  // you may include icons, below is a sample class:
+  myAwesomeToolbarButton.addClass('bensAwesomeToolbarButton');
+  myAwesomeToolbarButton.setToolTip('Bens Awesome extension');
+
+  // SubToolbar
+  this.subToolbar = (this.viewer.toolbar.getControl("MyAppToolbar") ?
+    this.viewer.toolbar.getControl("MyAppToolbar") :
+    new Autodesk.Viewing.UI.ControlGroup('MyAppToolbar'));
+  this.subToolbar.addControl(myAwesomeToolbarButton);
+
+  this.viewer.toolbar.addControl(this.subToolbar);
+};
+
+bensAwesomeExtension.prototype.unload = function () {
+  this.viewer.toolbar.removeControl(this.subToolbar);
+  return true;
+};
+
+Autodesk.Viewing.theExtensionManager.registerExtension('bensAwesomeExtension', bensAwesomeExtension);
